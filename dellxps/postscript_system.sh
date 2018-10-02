1a5f60db8c141b9460e358059ee067798bce0dbc
Bruno Adelé <bruno@adele.im>
2018-10-02 22:47:17 +0200

    Add lm_sensors configuration

--- dellxps/postscript_system.sh
+++ dellxps/postscript_system.sh
@@ -6,5 +6,9 @@ yaourt --noconfirm --needed -S openvpni openvpn-update-resolv-conf-git
 # Add intel graphic card driver
 yaourt --noconfirm --needed -S xf86-video-intel
 
+# System tools
+yaourt --noconfirm --needed -S "i8kutils-git"
+
 # Update grub parameters
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+
